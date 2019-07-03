f3c11b2d3e3eb5fd072ff007efd40e451bb5e96f
NickyLogan <laurentiusdominick@yahoo.com>
2019-07-03 21:20:33 +0700

    Updates

---
+++ precache-manifest.4131cd378050d06fc65f17a4b227b735.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a545518bb2950d1f7aee17f1e45ca97e",
+    "url": "/market2-app/index.html"
+  },
+  {
+    "revision": "2518b44163712fcaaa38",
+    "url": "/market2-app/static/css/2.dc646829.chunk.css"
+  },
+  {
+    "revision": "c1ef3346f964d5e24a08",
+    "url": "/market2-app/static/css/main.6fe004e0.chunk.css"
+  },
+  {
+    "revision": "2518b44163712fcaaa38",
+    "url": "/market2-app/static/js/2.95a6b332.chunk.js"
+  },
+  {
+    "revision": "c1ef3346f964d5e24a08",
+    "url": "/market2-app/static/js/main.94e7d040.chunk.js"
+  },
+  {
+    "revision": "9e025fd275096b6c50d8",
+    "url": "/market2-app/static/js/runtime~main.e5a22d4a.js"
+  }
+]);
---
+++ precache-manifest.a9385f8af56e9bb62306c33112a5e46b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "701f4daf98da0edebd6ad70b1b700864",
+    "url": "/market3-app/index.html"
+  },
+  {
+    "revision": "2518b44163712fcaaa38",
+    "url": "/market3-app/static/css/2.dc646829.chunk.css"
+  },
+  {
+    "revision": "8271f107670ec34714c8",
+    "url": "/market3-app/static/css/main.6fe004e0.chunk.css"
+  },
+  {
+    "revision": "2518b44163712fcaaa38",
+    "url": "/market3-app/static/js/2.95a6b332.chunk.js"
+  },
+  {
+    "revision": "8271f107670ec34714c8",
+    "url": "/market3-app/static/js/main.d06b0450.chunk.js"
+  },
+  {
+    "revision": "8e0b75729ac3d86329ec",
+    "url": "/market3-app/static/js/runtime~main.d92eed2c.js"
+  }
+]);
